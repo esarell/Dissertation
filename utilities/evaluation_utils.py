74328d74ac9c43f574215d83bc0dbd236d8e6139
Lizzie Sarell <sc18ess@leeds.ac.uk>
2022-02-24 13:04:17 +0000

    Attempt at using transquest QE

--- utilities/evaluation_utils.py
+++ utilities/evaluation_utils.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
-
-import sacrebleu
+import torch
+from transquest.algo.sentence_level.siamesetransquest.run_model import SiameseTransQuestModel
+#import sacrebleu
 
 
 def calculate_bleu(results: List[Dict[str, str]]) -> float:
@@ -10,11 +11,14 @@ def calculate_bleu(results: List[Dict[str, str]]) -> float:
     :param results: collection of dictionary with entries containing target sequences and generated sequences
     :return: BLEU score
     """
-
+    model = SiameseTransQuestModel("TransQuest/siamesetransquest-da-en_zh-wiki")
+    #model = SiameseTransQuestModel("xlmroberta","TransQuest/siamesetransquest-da-en_zh-wiki",labels=["OK", "BAD"], use_cuda=torch.cuda.is_available())
+    #model = MicroTransQuestModel("xlmroberta", "TransQuest/microtransquest-en_lv-pharmaceutical-nmt", labels=["OK", "BAD"], use_cuda=torch.cuda.is_available())
     candidates = [dct['decoded_sequence'] for dct in results]
-    print(candidates)
+    #print(candidates)
     references = [[dct['target_sequence'] for dct in results]]
-
-    bleu = sacrebleu.corpus_bleu(candidates, references)
+    predictions = model.predict([[candidates, references]])
+    print(predictions)
+    #bleu = sacrebleu.corpus_bleu(candidates, references)
 
     return bleu.score
