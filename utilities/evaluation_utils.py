75b5d2d86ca3a48ef3d92ee3612221cec742ff77
Lizzie Sarell <sc18ess@leeds.ac.uk>
2022-03-03 11:17:29 +0000

    working eval

--- utilities/evaluation_utils.py
+++ utilities/evaluation_utils.py
@@ -1,12 +1,12 @@
 from typing import List, Dict
-from unittest import result
-
-from matplotlib.font_manager import json_load
 #import torch
 #from transquest.algo.sentence_level.siamesetransquest.run_model import SiameseTransQuestModel
 import sacrebleu
-import json
+from typing import List, Dict
+from unittest import result
 
+#from matplotlib.font_manager import json_load
+import json
 
 def calculate_bleu(results: List[Dict[str, str]]) -> float:
     """
@@ -15,19 +15,17 @@ def calculate_bleu(results: List[Dict[str, str]]) -> float:
     :param results: collection of dictionary with entries containing target sequences and generated sequences
     :return: BLEU score
     """
-    model = SiameseTransQuestModel("TransQuest/siamesetransquest-da-en_zh-wiki")
+    '''model = SiameseTransQuestModel("TransQuest/siamesetransquest-da-en_zh-wiki")
     #model = SiameseTransQuestModel("xlmroberta","TransQuest/siamesetransquest-da-en_zh-wiki",labels=["OK", "BAD"], use_cuda=torch.cuda.is_available())
-    #model = MicroTransQuestModel("xlmroberta", "TransQuest/microtransquest-en_lv-pharmaceutical-nmt", labels=["OK", "BAD"], use_cuda=torch.cuda.is_available())
+    #model = MicroTransQuestModel("xlmroberta", "TransQuest/microtransquest-en_lv-pharmaceutical-nmt", labels=["OK", "BAD"], use_cuda=torch.cuda.is_available())'''
     candidates = [dct['decoded_sequence'] for dct in results]
     #print(candidates)
     references = [[dct['target_sequence'] for dct in results]]
-    predictions = model.predict([[candidates, references]])
-    print("TransQuest Result:"+predictions)
+    #predictions = model.predict([[candidates, references]])
+    #print("TransQuest Result:"+predictions)
     bleu = sacrebleu.corpus_bleu(candidates, references)
-    data = json_load(results)
+    #data = json.load(results)
     with open('QEdata.json', 'w') as f:
         json.dump(results, f)
     f.close()
-
-
     return bleu.score
